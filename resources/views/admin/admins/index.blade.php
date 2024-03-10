3f25444207a446947c92e9e9e7965467b5a67042
ZI WEI YEO <ziweiyeo@cspeed.com.my>
2024-03-10 22:35:56 +0800

    Add HasStatus trait

--- resources/views/admin/admins/index.blade.php
+++ resources/views/admin/admins/index.blade.php
@@ -39,7 +39,7 @@
                             {{ $admin->email }}
                         </td>
                         <td class="px-6 py-4">
-                            {{ $admin->status }}
+                            {!! $admin->status_badge !!}
                         </td>
                         <td class="px-6 py-4">
                             <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
